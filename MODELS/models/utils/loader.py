4403ba08e7ceb43e05f72f1937b8cebbc844026d
giahuytran4205 <tghuy23@clc.fitus.edu.vn>
2025-12-01 01:21:36 +0700

    Add loader for dataset

---
+++ MODELS/models/utils/loader.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def load_dataset(path: str):
+    """
+    Load the dataset from a NumPy .npz file.
+
+    Args:
+        path (str): The file path to the dataset.
+
+    Returns:
+        tuple: A tuple containing (X_train, y_train, X_test, y_test).
+    """
+    data = np.load(path)
+
+    X_train = np.array(data["X_train"])
+    y_train = np.array(data["y_train"])
+    X_test = np.array(data["X_test"])
+    y_test = np.array(data["y_test"])
+
+    return (X_train, y_train, X_test, y_test)
