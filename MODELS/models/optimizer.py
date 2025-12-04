bb885bde65e29218588a95e635d92f833129ec1e
giahuytran4205 <tghuy23@clc.fitus.edu.vn>
2025-12-05 01:08:08 +0700

    Update fit() for base class and update optimizer

--- MODELS/models/optimizer.py
+++ MODELS/models/optimizer.py
@@ -1,23 +1,29 @@
 import numpy as np
 
 class Optimizer:
-    def __init__(self):
-        pass
+    def __init__(self, weight_decay: float=None):
+        self.weight_decay = weight_decay
 
-    def __call__(self, grads):
+    def apply_grad(self, grad: np.ndarray) -> np.ndarray:
         raise NotImplementedError("This method should be overridden by subclasses.")
+
+    def apply(self, weights: np.ndarray, grad: np.ndarray, learning_rate: float) -> np.ndarray:
+        if self.weight_decay is not None:
+            grad += self.weight_decay * weights
+        update = self.apply_grad(grad)
+        return weights - learning_rate * update
     
 class Adam(Optimizer):
-    def __init__(self, beta1=0.9, beta2=0.999, epsilon=1e-8):
-        super().__init__()
-        self.t = 0
+    def __init__(self, weight_decay=None, beta1=0.9, beta2=0.999, epsilon=1e-8):
+        super().__init__(weight_decay)
         self.beta1 = beta1
         self.beta2 = beta2
         self.epsilon = epsilon
         self.m = None
         self.v = None
+        self.t = 0
 
-    def __call__(self, grad: np.ndarray) -> np.ndarray:
+    def apply_grad(self, grad: np.ndarray) -> np.ndarray:
         if self.m is None:
             self.m = np.zeros_like(grad)
             self.v = np.zeros_like(grad)
@@ -31,9 +37,36 @@ class Adam(Optimizer):
 
         return m_hat / (np.sqrt(v_hat) + self.epsilon)
     
-class SGD(Optimizer):
-    def __init__(self):
-        super().__init__()
+class GD(Optimizer):
+    def __init__(self, weight_decay=None):
+        super().__init__(weight_decay)
 
-    def __call__(self, grad: np.ndarray) -> np.ndarray:
+    def apply_grad(self, grad: np.ndarray) -> np.ndarray:
         return grad
+    
+class RMSProp(Optimizer):
+    def __init__(self, weight_decay=None, beta=0.9, epsilon=1e-8):
+        super().__init__(weight_decay)
+        self.beta = beta
+        self.epsilon = epsilon
+        self.v = None
+
+    def apply_grad(self, grad: np.ndarray) -> np.ndarray:
+        if self.v is None:
+            self.v = np.zeros_like(grad)
+
+        self.v = self.beta * self.v + (1 - self.beta) * (grad ** 2)
+        return grad / (np.sqrt(self.v) + self.epsilon)
+    
+class Adagrad(Optimizer):
+    def __init__(self, weight_decay=None, epsilon=1e-8):
+        super().__init__(weight_decay)
+        self.epsilon = epsilon
+        self.accumulated_grad = None
+
+    def apply_grad(self, grad: np.ndarray) -> np.ndarray:
+        if self.accumulated_grad is None:
+            self.accumulated_grad = np.zeros_like(grad)
+
+        self.accumulated_grad += grad ** 2
+        return grad / (np.sqrt(self.accumulated_grad) + self.epsilon)
