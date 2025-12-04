added block averaging and fixing overflow

MODELS/models/softmax_regression.py
@@ -29,7 +29,7 @@ class SoftmaxRegression:
         if self.weights is not None and resume:
             self.best_weights = self.weights.copy()
             return
-        self.weights = np.zeros((self.num_features + 1, self.num_classes))
+        self.weights = np.zeros((self.num_features + 1, self.num_classes), dtype=np.float32)
         self.best_weights = self.weights.copy()
 
     def get_X_biased(self, X: np.ndarray):

MODELS/models/utils/loader.py
@@ -12,9 +12,9 @@ def load_dataset(path: str):
     """
     data = np.load(path)
 
-    X_train = np.array(data["X_train"])
+    X_train = np.array(data["X_train"], dtype=np.float32)
     y_train = np.array(data["y_train"])
-    X_test = np.array(data["X_test"])
+    X_test = np.array(data["X_test"], dtype=np.float32)
     y_test = np.array(data["y_test"])
 
     return (X_train, y_train, X_test, y_test)